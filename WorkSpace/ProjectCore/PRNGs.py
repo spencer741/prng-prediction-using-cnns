0ef71463a5de2ee90e940fd05d709f31f206e2b7
spencer741 <sa.variable@gmail.com>
2020-03-23 15:31:34 -0500

    pulled over Ali's mid-square implementation

--- WorkSpace/ProjectCore/PRNGs.py
+++ WorkSpace/ProjectCore/PRNGs.py
@@ -25,8 +25,31 @@ It might return something like
 We will control parsing the n-length list and handling seeds externally. This plays logically with separation of concerns for our usecase.
 
 '''
+
+import sys
+
 def Middle_Square():
     print("Middle_Square")
+    seed_number = int(input("Please enter a four digit number:\n[####] "))
+    number = seed_number
+    already_seen = set()
+    counter = 0
+    count = 0
+    while number not in already_seen:
+        counter += 1
+        already_seen.add(number)
+        number = int(str(number * number).zfill(8)[2:6])  # zfill adds padding of zeroes
+        while (number > 0):
+            number = number//10
+            count = count + 1
+            #print (count)
+            #print (number)
+            if (count % 10 != 0):
+                print(f"{number}", end = '')
+            elif (count % 10 == 0):
+                print(f"{number}")
+
+        
     
 def Lehmer():
     print("Lehmer")
