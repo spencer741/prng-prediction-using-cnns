3e0830829380c903ff86f0508d213e36a4b7620d
spencer741 <sa.variable@gmail.com>
2020-03-24 16:09:39 -0500

    worked on mid-square implementation and project proposal

--- WorkSpace/ProjectCore/PRNGs.py
+++ WorkSpace/ProjectCore/PRNGs.py
@@ -28,28 +28,60 @@ We will control parsing the n-length list and handling seeds externally. This pl
 
 import sys
 
-def Middle_Square():
+def Middle_Square(seed, listlength):
     print("Middle_Square")
-    seed_number = int(input("Please enter a four digit number:\n[####] "))
+    
+    #Create a list of length listlength
+    numlist = []
+    
+    
+    #seed_number = int(input("Please enter a four digit number:\n[####] "))
+    #number = seed_number
+    
+    
+    for i in range(listlength):
+        #list that is one number, broken up into digits, since we are generating a digit at a time. the length is based on the seed.
+        tempnumlist = []
+        #initializers for inner looping to work
+        already_seen = set()
+        counter = 0
+        count = 0
+        number = 0
+        while number not in already_seen:
+            counter += 1
+            already_seen.add(number)
+            number = int(str(seed * seed).zfill(8)[2:6])  # zfill adds padding of zeroes
+            while (number > 0):
+                number = number//10
+                count = count + 1
+                #print (count)
+                #print (number)
+                if (count % 10 != 0):
+                    tempnumlist.append(number)
+                    #print(f"{number}", end = '')
+                elif (count % 10 == 0):
+                    tempnumlist.append(number)
+                    #print(f"{number}")
+        numlist.append(int(''.join(map(str,tempnumlist)))) #add tempnumlist to numlist as one number.
+        
+    print("numlist", numlist)
+      
+        
+def midsquare(seed, listlength):
+    seed_number = int(seed)
     number = seed_number
     already_seen = set()
     counter = 0
-    count = 0
+
     while number not in already_seen:
         counter += 1
         already_seen.add(number)
         number = int(str(number * number).zfill(8)[2:6])  # zfill adds padding of zeroes
-        while (number > 0):
-            number = number//10
-            count = count + 1
-            #print (count)
-            #print (number)
-            if (count % 10 != 0):
-                print(f"{number}", end = '')
-            elif (count % 10 == 0):
-                print(f"{number}")
+        print(f"#{counter}: {number}")
 
-        
+    print(f"We began with {seed_number}, and"
+          f" have repeated ourselves after {counter} steps"
+          f" with {number}.")
     
 def Lehmer():
     print("Lehmer")
